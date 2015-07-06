bd904aeb631bc691a2504ee060ac6ac86b40583f
dave45678 <dave45678@gmail.com>
2015-07-06 12:49:33 -0400

    First commit

---
+++ MyFirstJSPProject/src/com/tutorial/LoginBean.java
@@ -0,0 +1,29 @@
+/**
+ * LoginBean.java
+ * 07.01.2015
+ * djw
+ */
+
+package com.tutorial;
+
+public class LoginBean {
+	private String name;
+	private String password;
+
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(final String value)
+	{
+		name = value;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(final String value)
+	{
+		password = value;
+	}
+}
